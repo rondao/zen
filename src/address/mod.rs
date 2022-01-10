281124c84d5db600665aeffc2650a842f411a547
Rafael Rondao <rafael.rondao@gmail.com>
2022-01-10 20:09:08 -0300

    Add Pc to LoRom convertion
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/address/mod.rs
+++ src/address/mod.rs
@@ -1,10 +1,10 @@
 // Reference: https://en.m.wikibooks.org/wiki/Super_NES_Programming/SNES_memory_map
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct LoRom {
     pub address: usize,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Pc {
     pub address: usize,
 }
@@ -21,6 +21,20 @@ impl From<LoRom> for Pc {
 
         Pc {
             address: number_of_banks * 0x8000 + offset,
+        }
+    }
+}
+
+impl From<Pc> for LoRom {
+    fn from(pc: Pc) -> Self {
+        // Each $8000 address in Pc is a bank in LoRom.
+        let number_of_banks = pc.address / 0x8000;
+
+        // Less then $8000 will be an offset after $8000 in the LoRom.
+        let offset = pc.address % 0x00_8000;
+
+        LoRom {
+            address: 0x80_0000 + (number_of_banks * 0x01_0000) + 0x8000 + offset,
         }
     }
 }
