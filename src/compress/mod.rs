21d05e71f9a5b25913658fe61fc5cf4c4ef055c8
Rafael Rondao <rafael.rondao@gmail.com>
2022-01-13 19:06:45 -0300

    Add tests for Lz5 compression and decompression
    Tests done with external files for Copyright reasons.
    
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/compress/mod.rs
+++ src/compress/mod.rs
@@ -21,3 +21,57 @@ impl fmt::Debug for Lz5Error {
         write!(f, "Failed to decompress data using LZ5 algorithm.")
     }
 }
+
+mod tests {
+    use super::*;
+
+    /// Test the Lz5 decompression.
+    #[test]
+    fn decompress_files_with_lz5() {
+        let test_dir = "/home/rondao/dev/snes_data/test";
+        let test_cases = [
+            "BAC629.gfx",
+            "BEE78D.gfx",
+            "C1B6F6.ttb",
+            "C2AD7C.tpl",
+            "C2B5E4.tpl",
+            "C2C2BB.lvl",
+            "C2855F.ttb",
+            "CC82A8.lvl",
+        ];
+
+        for filename in test_cases {
+            let decompressed_data =
+                lz5_decompress(&std::fs::read(format!("{}/{}.bin", test_dir, filename)).unwrap())
+                    .unwrap();
+            let expected_data = std::fs::read(format!("{}/{}", test_dir, filename)).unwrap();
+            assert_eq!(decompressed_data, expected_data);
+        }
+    }
+
+    /// Test the Lz5 compression.
+    /// Certify that we can obtain the same data by decompressing it.
+    #[test]
+    fn compress_files_with_lz5() {
+        let test_dir = "/home/rondao/dev/snes_data/test";
+        let test_cases = [
+            "BAC629.gfx",
+            "BEE78D.gfx",
+            "C1B6F6.ttb",
+            "C2AD7C.tpl",
+            "C2B5E4.tpl",
+            "C2C2BB.lvl",
+            "C2855F.ttb",
+            "CC82A8.lvl",
+        ];
+
+        for filename in test_cases {
+            let data = std::fs::read(format!("{}/{}", test_dir, filename)).unwrap();
+
+            let compressed_data = lz5_compress(&data);
+            let decompressed_data = lz5_decompress(&compressed_data).unwrap();
+
+            assert_eq!(decompressed_data, data);
+        }
+    }
+}
