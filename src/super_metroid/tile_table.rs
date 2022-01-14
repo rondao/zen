fc501f143c91b356756ba56e54bb27efbd624de6
Rafael Rondao <rafael.rondao@gmail.com>
2022-01-14 13:04:40 -0300

    Test loading a Tile Table from bytes
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/super_metroid/tile_table.rs
+++ src/super_metroid/tile_table.rs
@@ -8,7 +8,7 @@ pub const BLOCK_SIZE: usize = TILE_SIZE * 2;
 
 pub type TileTable = Vec<Tile>;
 
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
 pub struct Tile {
     pub y_flip: bool,
     pub x_flip: bool,
@@ -32,4 +32,53 @@ pub fn from_bytes(source: &[u8]) -> TileTable {
             }
         })
         .collect()
+}
+
+mod tests {
+    use super::*;
+
+    /// Load a tile table from bytes, each tile is two bytes.
+    #[test]
+    fn load_tile_table_from_bytes() {
+        let data: Vec<u8> = [
+            [0b0000_0000, 0b0_0_0_000_00],
+            [0b1010_0101, 0b0_1_0_101_01],
+            [0b0101_1010, 0b1_0_1_010_10],
+            [0b1111_1111, 0b1_1_1_111_11],
+        ]
+        .concat();
+
+        let expected_tile_table = [
+            Tile {
+                y_flip: false,
+                x_flip: false,
+                unknown: false,
+                sub_palette: 0,
+                gfx_index: 0,
+            },
+            Tile {
+                y_flip: false,
+                x_flip: true,
+                unknown: false,
+                sub_palette: 0b101,
+                gfx_index: 0b01_1010_0101,
+            },
+            Tile {
+                y_flip: true,
+                x_flip: false,
+                unknown: true,
+                sub_palette: 0b010,
+                gfx_index: 0b10_0101_1010,
+            },
+            Tile {
+                y_flip: true,
+                x_flip: true,
+                unknown: true,
+                sub_palette: 0b111,
+                gfx_index: 0b11_1111_1111,
+            },
+        ];
+
+        assert_eq!(from_bytes(&data), expected_tile_table);
+    }
 }
